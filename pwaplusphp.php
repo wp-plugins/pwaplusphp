f19f0e88d6ee240485dfec254c99a9ecf157ce54
smccandl <smccandl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-26 16:19:35 +0000

    Added empty float-left div to ensure layout is not broken
    git-svn-id: https://plugins.svn.wordpress.org/pwaplusphp/trunk@221873 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pwaplusphp.php
+++ pwaplusphp.php
@@ -268,6 +268,7 @@ if ($SHOW_FOOTER == "TRUE") {
 	#$out .= "</div>" . "\n";
 #   }
 
+$out .= "<div style='float: left' />"; # Ensure PWA+PHP doesn't break theme layout
 return $out;
 }
 
@@ -645,6 +646,7 @@ foreach ($vals as $val) {
 	#
 	#}
 
+	$out .= "<div style='float: left' />"; # Ensure PWA+PHP doesn't break theme layout
 	return($out);
 
 }
