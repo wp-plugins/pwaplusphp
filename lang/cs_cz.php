6a0c1c96eae18c5236a8ccf0ab1fcc2117fdfa7e
smccandl <smccandl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-11 03:05:05 +0000

    Adding Czech language file
    git-svn-id: https://plugins.svn.wordpress.org/pwaplusphp/trunk@343909 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/cs_cz.php
@@ -0,0 +1,21 @@
+<?PHP
+#-----------------------------------------------------------------------------------------
+# CZECH LANGUAGE FILE FOR PWA+PHP
+#-----------------------------------------------------------------------------------------
+$LANG_MISSING_VAR_H1="Chyba: Jedna nebo více povinných proměnných z config.php chybí!";
+$LANG_MISSING_VAR_H3="Prosím spusťte znovu konfigurační skript install.php.";
+$LANG_PERM_FILTER="Přístup odepřen.  Vyžadován filtr.";
+$LANG_GALLERIES="Picasa galerie";
+$LANG_GALLERY="Galerie";
+$LANG_IMAGES="obrázky";
+$LANG_PRIVATE="Soukromé";
+$LANG_PUBLIC="Veřejné";
+$LANG_WHERE="Umístění";
+$LANG_ACCESS="Přístup";
+$LANG_PHOTOS_IN="fotografie v";
+$LANG_ALBUMS="alba";
+$LANG_BACK="zpět na seznam alb";
+$LANG_PAGE="Stránka";
+$LANG_GET="Přijmout";
+$LANG_GENERATED="Stránka vytvořena";
+?>
