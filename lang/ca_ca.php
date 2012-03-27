4ebb47e0559b057e9d190368b62c1279837c9a68
smccandl <smccandl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-27 01:28:37 +0000

    Added Catalan Language file
    git-svn-id: https://plugins.svn.wordpress.org/pwaplusphp/trunk@524097 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/ca_ca.php
@@ -0,0 +1,21 @@
+﻿<?PHP
+#-----------------------------------------------------------------------------------------
+# CATALAN LANGUAGE FILE FOR PWA+PHP
+#-----------------------------------------------------------------------------------------
+$LANG_MISSING_VAR_H1="Error: Falta una o m&eacute;s variables necessàries al config.php!";
+$LANG_MISSING_VAR_H3="Si us plau, torna a executar l'script de configuraci&oacute; install.php.";
+$LANG_PERM_FILTER="Perm&iacute;s denegat. Cal un filtre.";
+$LANG_GALLERIES="Galeries Picasa";
+$LANG_GALLERY="Galeria";
+$LANG_IMAGES="imatges";
+$LANG_PRIVATE="Privat";
+$LANG_PUBLIC="P&uacute;blic";
+$LANG_WHERE="Lloc";
+$LANG_ACCESS="Acc&eacute;s";
+$LANG_PHOTOS_IN="fotos en";
+$LANG_ALBUMS="&agrave;lbums";
+$LANG_BACK="torna a la llista d'&agrave;lbums";
+$LANG_PAGE="P&agrave;gina";
+$LANG_GET="Obtenir";
+$LANG_GENERATED="P&agrave;gina generada per";
+?>
